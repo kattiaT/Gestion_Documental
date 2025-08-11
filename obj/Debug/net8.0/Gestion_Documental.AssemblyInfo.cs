255cef6cd129289c12007ba07df90fe812bbd4aa
kattiaT <168695823+kattiaT@users.noreply.github.com>
2025-08-11 16:22:24 -0600

    instalacion de 2 paquetes

--- obj/Debug/net8.0/Gestion_Documental.AssemblyInfo.cs
+++ obj/Debug/net8.0/Gestion_Documental.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Gestion_Documental")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f896e4a294c02d15e8cdc791a013c9ebf842cf3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Gestion_Documental")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Gestion_Documental")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
