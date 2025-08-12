c240e1904bdc7f6857b6a6f2fb44124ea02dc73c
kattiaT <katt.hkj@gmail.com>
2025-08-12 16:00:32 -0600

    llenando  demo controlador

--- Controllers/DocumentoController.cs
+++ Controllers/DocumentoController.cs
@@ -1,32 +1,14 @@
-using System.Diagnostics;
-using Gestion_Documental.Models;
 using Microsoft.AspNetCore.Mvc;
 
-namespace Gestion_Documental.Controllers
+public class DocumentoController : Controller
 {
-    public class DocumentoController : Controller
+    public IActionResult Index()
     {
-        private readonly ILogger<DocumentoController> _logger;
-
-        public DocumentoController(ILogger<DocumentoController> logger)
-        {
-            _logger = logger;
-        }
-
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+        return View();
     }
+
+    private static readonly List<(string Titulo, string Url, string Tipo)> _demo = new()
+    {
+        
+    };
 }
